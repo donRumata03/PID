636683055bb3667946da94b7aec93f7a1661fee4
donRumata <donrumata03@gmail.com>
2020-02-14 14:22:39 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,32 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def sgn(x):
+    return 1 if x > 0 else (0 if x == 0 else -1)
+
+
 lag = 0.01
 length = 2000.0
 x = np.arange(0.0, length, lag)
 
 c = (np.sin(x/4) * 0.05 + np.sin(x/10)*0.1 + np.sin(x/100) + np.cos(x/1000))
-# c = x
-#c = np.array([1000] * int(length / lag))
 l = [0]
 
 x2 = np.arange(len(l))
 ms = []
 plt.plot(x, c)
+
+Kp = 0.01
+Ki = 0
+Kd = 10
+
+max_speed = 0.001
+
+
+max_actual_speed = 0
+
 for i in l:
-    Kp = 9050.0
-    Ki = 0
-    Kd = 200000
 
     S = 0.0
     s = 0
 
-    Sentensivity = 20000.0
+    sentensivity = 2000.0
     out = 0.0
     C = 5.0
     Elast = c[0] - out
     Speed = 0
-    max_power = 0.001
+    max_power = 0.00001
     outs = []
     ##############################
     # Out = Integral(Integral(U))#
@@ -42,20 +52,15 @@ for i in l:
         else:
             U = min(U, max_power)
 
-        Speed += U / Sentensivity
+        Speed += U / sentensivity
+        Speed = sgn(Speed) * min(abs(Speed), max_speed)
+        max_actual_speed = max(max_actual_speed, Speed)
         outs.append(out)
         out += Speed
         Elast = e
 
     plt.plot(x, outs)
-    # plt.plot(x,-np.cos(x*np.pi/400)*1000+1000)
-    # plt.plot(np.array([0]), np.array([0]))
     print(abs(s))
-    # plt.grid(True)
-
-
-
+    print(max_actual_speed)
 
 plt.show()
-
-
